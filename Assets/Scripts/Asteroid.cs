6168bca9e65fe8c3eda2413cf3036d6d7002fabd
Andrey Vorobyov <andrey.vorobyov@intel.com>
2017-04-02 21:05:30 +0300

    Splitting asteroid to fragments

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -3,5 +3,20 @@ using System.Collections;
 
 public class Asteroid : SpaceObject
 {
+    public GameObject fragment;
 
+    [SerializeField]
+    private int fragmentCount = 2;
+
+    protected override void Explode()
+    {
+        for (int i = 0; i < fragmentCount; ++i)
+        {
+            Vector2 direction = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+            GameObject clone = Instantiate(fragment, transform.position, Quaternion.identity) as GameObject;
+            Rigidbody2D rb = clone.GetComponent<Rigidbody2D>();
+            rb.AddForce(direction * 5.0f, ForceMode2D.Impulse);
+        }
+        base.Explode();
+    }
 }
--- Assets/Scripts/SpaceObject.cs
+++ Assets/Scripts/SpaceObject.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class SpaceObject : MonoBehaviour
 {
-    protected void Explode()
+    protected virtual void Explode()
     {
         // TODO: show explode
         Destroy(gameObject);
