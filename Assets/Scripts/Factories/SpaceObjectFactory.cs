8234fe4be26a66ab169741cd02d29c7da573c541
AndreyTerkin <melnik1993@gmail.com>
2017-11-08 23:31:25 +0300

    Space object factory separation

---
+++ Assets/Scripts/Factories/SpaceObjectFactory.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Factories
+{
+    class SpaceObjectFactory : MonoBehaviour
+    {
+        protected Transform _transform;
+        protected Border _border;
+
+        private static float _errorOffset = 0.25f;
+
+        public SpaceObjectFactory(Transform transform, Border border)
+        {
+            _transform = transform;
+            _border = border;
+        }
+
+        public GameObject Create(GameObject gameObject, ref Vector2 direction)
+        {
+            BoxCollider2D collider = gameObject.GetComponent<BoxCollider2D>();
+            Vector3 position = new Vector3();
+            InitSpawnParameters(collider, ref position, ref direction);
+            return Instantiate(gameObject, position, Quaternion.identity) as GameObject;
+        }
+
+        protected virtual void InitSpawnParameters(BoxCollider2D collider, ref Vector3 position, ref Vector2 direction)
+        {
+            float deflection = Random.Range(-1.0f, 1.0f);
+            float side = Random.Range(0.0f, 4.0f);
+
+            if (side >= 0 && side < 1) // top
+            {
+                position = new Vector3(Random.Range(-_border.borderXmax, _border.borderXmax),
+                                       _border.borderYmax + collider.size.y,
+                                       0.0f);
+                direction = -_transform.up + new Vector3(deflection, 0.0f);
+            }
+            else if (side >= 1 && side < 2) // bottom
+            {
+                position = new Vector3(Random.Range(-_border.borderXmax, _border.borderXmax),
+                                       _border.borderYmin - collider.size.y - _errorOffset,
+                                       0.0f);
+                direction = _transform.up + new Vector3(deflection, 0.0f);
+            }
+            else if (side >= 2 && side < 3) // right
+            {
+                position = new Vector3(_border.borderXmax + collider.size.x,
+                                       Random.Range(-_border.borderYmax, _border.borderYmax),
+                                       0.0f);
+                direction = -_transform.right + new Vector3(0.0f, deflection);
+            }
+            else if (side >= 3 && side < 4) // left
+            {
+                position = new Vector3(_border.borderXmin - collider.size.x,
+                                       Random.Range(-_border.borderYmax, _border.borderYmax),
+                                       0.0f);
+                direction = _transform.right + new Vector3(0.0f, deflection);
+            }
+
+            Debug.Log("Collider width: " + collider.size.x);
+            Debug.Log("Spawning: " + _border.borderXmax + " " + _border.borderXmin);
+        }
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+using Assets.Scripts.Factories;
 
 public class GameController : MonoBehaviour
 {
     public GameObject asteroid;
     public GameObject ufo;
+
+    private SpaceObjectFactory _spaceObjectFactory;
 
     private Border border;
 
@@ -31,6 +36,8 @@ public class GameController : MonoBehaviour
         Boundary boundary = FindObjectOfType<Boundary>();
         if (boundary)
             border = boundary.border;
+
+        _spaceObjectFactory = new SpaceObjectFactory(transform, border);
 
         StartCoroutine(SpawnAsteroid());
         StartCoroutine(SpawnUfo());
@@ -70,69 +77,17 @@ public class GameController : MonoBehaviour
 
     void AsteroidFactory()
     {
-        BoxCollider2D collider = asteroid.GetComponent<BoxCollider2D>();
-
-        Vector3 position = new Vector3();
-        Vector2 direction = new Vector2();
-
-        InitSpawnParameters(collider, ref position, ref direction);
-
-        GameObject clone = Instantiate(asteroid, position, Quaternion.identity) as GameObject;
+        Vector2 direction = new Vector2(1.0f, 1.0f);
+        GameObject clone = _spaceObjectFactory.Create(asteroid, ref direction);
         Rigidbody2D rb = clone.GetComponent<Rigidbody2D>();
         rb.AddForce(direction * 3.0f, ForceMode2D.Impulse);
     }
 
     void UfoFactory()
     {
-        BoxCollider2D collider = ufo.GetComponent<BoxCollider2D>();
-
-        Vector3 position = new Vector3();
-        Vector2 direction = new Vector2();
-
-        InitSpawnParameters(collider, ref position, ref direction);
-
-        Debug.Log("factory: " + position + " " + direction);
-
-        GameObject clone = Instantiate(ufo, position, Quaternion.identity) as GameObject;
+        Vector2 direction = new Vector2(1.0f, 1.0f);
+        GameObject clone = _spaceObjectFactory.Create(ufo, ref direction);
         Rigidbody2D rb = clone.GetComponent<Rigidbody2D>();
         rb.AddForce(direction * 2.0f, ForceMode2D.Impulse);
-    }
-
-    void InitSpawnParameters(BoxCollider2D collider, ref Vector3 position, ref Vector2 direction)
-    {
-        float deflection = Random.Range(-1.0f, 1.0f);
-        float side = Random.Range(0.0f, 4.0f);
-        
-        if (side >= 0 && side < 1) // top
-        {
-            position = new Vector3(Random.Range(-border.borderXmax, border.borderXmax),
-                                   border.borderYmax + collider.size.y,
-                                   0.0f);
-            direction = -transform.up + new Vector3(deflection, 0.0f);
-        }
-        else if (side >= 1 && side < 2) // bottom
-        {
-            position = new Vector3(Random.Range(-border.borderXmax, border.borderXmax),
-                                   border.borderYmin - collider.size.y - errorOffset,
-                                   0.0f);
-            direction = transform.up + new Vector3(deflection, 0.0f);
-        }
-        else if (side >= 2 && side < 3) // right
-        {
-            position = new Vector3(border.borderXmax + collider.size.x,
-                                   Random.Range(-border.borderYmax, border.borderYmax),
-                                   0.0f);
-            direction = -transform.right + new Vector3(0.0f, deflection);
-        }
-        else if (side >= 3 && side < 4) // left
-        {
-            position = new Vector3(border.borderXmin - collider.size.x,
-                                   Random.Range(-border.borderYmax, border.borderYmax),
-                                   0.0f);
-            direction = transform.right + new Vector3(0.0f, deflection);
-        }
-
-        Debug.Log("Collider width: " + collider.size.x);
-        Debug.Log("Spawning: " + border.borderXmax + " " + border.borderXmin);
     }
 }
